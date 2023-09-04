23d9868bfdc020a908aea17b0daeee5b6ac37759
John Michael Basalo <jomaikoh+github@gmail.com>
2023-09-04 20:37:19 +0800

    updated main.js operators

--- 19-problem-solving/main.js
+++ 19-problem-solving/main.js
@@ -3,11 +3,11 @@ let userInput = parseInt(prompt("Enter a number"));
 
 //Loop from 1 to the entered number
 for (let i = 1; i <= userInput; i++)
-  if (i % 3 == 0 && i % 5 == 0) {
+  if (i % 3 === 0 && i % 5 === 0) {
     console.log("fizzbuzz");
-  } else if (i % 5 == 0) {
+  } else if (i % 5 === 0) {
     console.log("buzz");
-  } else if (i % 3 == 0) {
+  } else if (i % 3 === 0) {
     console.log("fizz");
   } else {
     console.log(i);
