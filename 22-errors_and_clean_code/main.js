34f8810c4b95e6db8a30315e0f4a5b95ac44eb05
John Michael Basalo <jomaikoh+github@gmail.com>
2023-09-07 19:52:28 +0800

    updated main js on errors

---
+++ 22-errors_and_clean_code/main.js
@@ -0,0 +1,23 @@
+//Errors and Clean Code
+
+//Reference Error - variable not defined
+
+// let a = "Hello";
+// let b = "World";
+// console.log(c);
+
+// //Syntax Error - missing parenthesis
+// function printHelloWorld() {
+//   console.log"Hello World!";
+// }
+// printHelloWorld();
+
+//Type Error - variable is not a string
+// -> an operand or argument passed to a function is not of the expected type by that operator or function
+// -> or when attempting to modify a value that cannot be changed;
+// -> or when attempting to use a value in a way that is not valid.
+
+// let str1 = "Hello";
+// let str2 = "World";
+// let message = str1.push(str2); //push is only used in an array which is str1 is not
+ 
