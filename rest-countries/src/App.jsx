3de4fb6f489bca0c257828edbf44ed3a5020134b
John Michael Basalo <jomaikoh+github@gmail.com>
2023-10-25 16:46:57 +0800

    update files and folders

---
+++ rest-countries/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect
+
+  return (
+    <div>
+      <div>
+        find countries <input />
+      </div>
+    </div>
+  );
+}
+
+export default App;
